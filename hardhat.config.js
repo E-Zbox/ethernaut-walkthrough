89950b776b305901e1ff2f5b89d5b5eb1a432256
E-Zbox <zboxstudios97@gmail.com>
2023-08-02 23:13:18 +0100

    hardhat configuration

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+
+// ENV variables
+const { INFURA_SEPOLIA_HTTP, PRIVATE_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.19",
+    networks: {
+        sepolia: {
+            accounts: [PRIVATE_KEY],
+            url: INFURA_SEPOLIA_HTTP,
+        },
+    },
+};
