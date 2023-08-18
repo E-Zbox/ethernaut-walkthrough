1c1b2cfd11a8cbb45631bd7e35107a3782609f3b
E-Zbox <zboxstudios97@gmail.com>
2023-08-18 01:41:49 +0100

    refactored emojis array to emojis object

--- test/index.js
+++ test/index.js
@@ -1,1 +1,14 @@
-exports.scriptEmojis = ["💻", "🚀", "⚔", "🏳", "👑", "🥘", "📜"];
+exports.scriptEmoji = ["💻", "🚀", "⚔", "🏳", "👑", "🥘", "📜"];
+
+exports.scriptEmojis = {
+    system: "💻",
+    launch: "🚀",
+    mobilize: "⚔",
+    flag: "🏳",
+    padlock: "🔓",
+    crown: "👑",
+    coin: "🥘",
+    loop: "💱 💰",
+    withdraw: "🏧",
+    contract: "📜",
+};
